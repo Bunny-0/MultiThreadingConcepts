e3c5eabe3344cca34d29ac66cf9e7d4e7ed6de12
shubham gaurav <shubhamgaurav2@gmail.com>
2025-07-23 00:27:22 +0530

    Executor Framework concept

---
+++ src/main/java/org/example/Threading_2_0/Executors/ExecutorFrameWork.java
@@ -0,0 +1,44 @@
+package org.example.Threading_2_0.Executors;
+
+import java.util.concurrent.*;
+
+public class ExecutorFrameWork {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        long startTime = System.currentTimeMillis();
+       ExecutorService executorService= Executors.newFixedThreadPool(3);
+
+       for(int i=1;i<9;i++){
+           int finalI = i;
+
+           Future<?> future = executorService.submit(() -> {
+            long data = factorial(finalI);
+            System.out.println("Factorial of " + finalI + " is: " + data);
+       });
+
+           System.out.println("Total time " + (System.currentTimeMillis() - startTime));
+
+       }
+        executorService.shutdown();
+
+       while(!executorService.awaitTermination(10, TimeUnit.MILLISECONDS)){
+           System.out.println("Waiting for tasks to complete...");
+       }
+
+
+
+    }
+
+    private static long factorial(int n) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
