84ec68a70baf5613f234ce4b5d18bf1dd2bf7bae
shubham gaurav <shubhamgaurav2@gmail.com>
2025-09-03 21:58:31 +0530

    miscellaneous topics

--- src/main/java/org/example/Revision/Iterator/interativeClass.java
+++ src/main/java/org/example/Revision/Iterator/interativeClass.java
@@ -1,15 +1,38 @@
 package org.example.Revision.Iterator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class interativeClass {
 
 
     public static void main(String[] args) {
 
+//
+//        List<Transaction> transactionList=new ArrayList<>();
+//        transactionList.add(new Transaction(1,1000,"CREDIT"));
+//        transactionList.add(new Transaction(2,2000,"DEBIT"));
+//        transactionList.add(new Transaction(3,3000,"CREDIT"));
+//        transactionList.add(new Transaction(4,4000,"DEBIT"));
+//
+//        ListIterator<Transaction> listIterator= transactionList.listIterator();
+//
+//        while(listIterator.hasNext()){
+//            System.out.println(listIterator.next());
+//            transactionList.add(new Transaction(5,5000,"CREDIT"));
+//        }
 
-        List<Transaction> transactionList=new ArrayList<>();
+        List<Transaction> transactionList=new CopyOnWriteArrayList<>();
+        transactionList.add(new Transaction(1,1000,"CREDIT"));
+        transactionList.add(new Transaction(2,2000,"DEBIT"));
+        transactionList.add(new Transaction(3,3000,"CREDIT"));
+        transactionList.add(new Transaction(4,4000,"DEBIT"));
+
+        Iterator<Transaction> iterator= transactionList.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+            transactionList.add(new Transaction(5,5000,"CREDIT"));
+
+        }
     }
 }
